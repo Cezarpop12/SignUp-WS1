0350d16fcbffdf823ad691dfd3b01411aa81c86a
Cezar <mr.chaser31@yahoo.com>
2024-06-03 23:40:18 +0200

    Sonar test 3
    Sonar test 3

--- src/test/java/com/SignUp/Controller/SignUpControllerTest.java
+++ src/test/java/com/SignUp/Controller/SignUpControllerTest.java
@@ -58,4 +58,68 @@ public class SignUpControllerTest {
                         .content("{\"name\": \"John\", \"username\": \"john123\", \"password\": \"password1\", \"email\": \"invalidemail\"}"))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    void createUser_HtmlCodeInName_ShouldReturnBadRequest() throws Exception {
+        mockMvc.perform(post("/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"<script>alert('hello');</script>\", \"username\": \"john123\", \"password\": \"password1\", \"email\": \"john@example.com\"}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createUser_HtmlCodeInUsername_ShouldReturnBadRequest() throws Exception {
+        mockMvc.perform(post("/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"John\", \"username\": \"<script>alert('hello');</script>\", \"password\": \"password1\", \"email\": \"john@example.com\"}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createUser_HtmlCodeInPassword_ShouldReturnBadRequest() throws Exception {
+        mockMvc.perform(post("/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"John\", \"username\": \"john123\", \"password\": \"<script>alert('hello');</script>\", \"email\": \"john@example.com\"}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createUser_HtmlCodeInEmail_ShouldReturnBadRequest() throws Exception {
+        mockMvc.perform(post("/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"John\", \"username\": \"john123\", \"password\": \"password1\", \"email\": \"<script>alert('hello');</script>\"}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createUser_QueryInName_ShouldReturnBadRequest() throws Exception {
+        mockMvc.perform(post("/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"SELECT * FROM users\", \"username\": \"john123\", \"password\": \"password1\", \"email\": \"john@example.com\"}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createUser_QueryInUsername_ShouldReturnBadRequest() throws Exception {
+        mockMvc.perform(post("/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"John\", \"username\": \"SELECT * FROM users\", \"password\": \"password1\", \"email\": \"john@example.com\"}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createUser_QueryInPassword_ShouldReturnBadRequest() throws Exception {
+        mockMvc.perform(post("/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"John\", \"username\": \"john123\", \"password\": \"SELECT * FROM users\", \"email\": \"john@example.com\"}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createUser_QueryInEmail_ShouldReturnBadRequest() throws Exception {
+        mockMvc.perform(post("/signUp")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\": \"John\", \"username\": \"john123\", \"password\": \"password1\", \"email\": \"SELECT * FROM users\"}"))
+                .andExpect(status().isBadRequest());
+    }
 }
